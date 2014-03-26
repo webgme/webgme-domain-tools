b2b40c8f448fdc07cc8f31a39547700be5a50254
pmeijer <patrik85@isis.vanderbilt.edu>
2014-03-26 15:16:33 -0500

    Added simple interpreter that log children.

---
+++ src/plugins/CyPhyLight/Children/Children.js
@@ -0,0 +1,43 @@
+/**
+ * Created by pmeijer on 3/26/2014.
+ */
+define(['src/PluginManager/PluginConfig',
+        'src/PluginManager/PluginBase'], function (PluginConfig, PluginBase) {
+
+    'use strict';
+
+    var ChildrenPlugin = function () {};
+
+    ChildrenPlugin.prototype = Object.create(PluginBase.prototype);
+
+    ChildrenPlugin.getDefaultConfig = function () {
+        return new PluginConfig();
+    };
+
+    ChildrenPlugin.prototype.main = function (config, callback) {
+        var core = config.core,
+            selectedNode = config.selectedNode;
+
+        core.loadChildren(selectedNode, function (err, childNodes) {
+            var i;
+            console.log('%j has children::', core.getAttribute(selectedNode, 'name'));
+
+            for (i = 0; i < childNodes.length; i += 1) {
+                console.log('  - %j', core.getAttribute(childNodes[i], 'name'));
+            }
+
+            if (callback) {
+                callback({'success': true});
+            }
+        });
+
+
+    };
+
+    ChildrenPlugin.prototype.doGUIConfig = function (preconfig, callback) {
+        callback({});
+    };
+
+
+    return ChildrenPlugin;
+});
