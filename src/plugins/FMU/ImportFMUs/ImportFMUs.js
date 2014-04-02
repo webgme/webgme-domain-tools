4c22e6a14b2856afd2ef41da88d03c9bf5cb95a4
James Klingler <jklingler@isis.vanderbilt.edu>
2014-04-02 16:40:45 -0500

    add json description of FMUs to import

--- src/plugins/FMU/ImportFMUs/ImportFMUs.js
+++ src/plugins/FMU/ImportFMUs/ImportFMUs.js
@@ -4,8 +4,9 @@
 
 define(['plugin/PluginConfig',
         'plugin/PluginBase',
-        'plugin/ImportFMUs/ImportFMUs/FMU'],
-    function (PluginConfig, PluginBase, FMU) {
+        'plugin/ImportFMUs/ImportFMUs/FMU',
+        'plugin/ImportFMUs/ImportFMUs/resources/modelica_components'],
+    function (PluginConfig, PluginBase, FMU, ComponentData) {
     'use strict';
 
     var ImportFMUsPlugin = function () {
@@ -30,20 +31,17 @@ define(['plugin/PluginConfig',
             core = this.core,
             rootNode = this.rootNode;
 
-        // FIXME: this is a hack to get intellisense
-//        var name;
-//        for (name in config.META) {
-//            if (config.META.hasOwnProperty(name)) {
-//                FMU[name] = config.META[name];
-//            }
-//        }
-
+        // get all 'possible' object types from the MetaModel
         this.updateMETA(FMU);
 
+        // create an FCO within the rootNode, which is an instance of FMU.FMU_Library
         var newFmuLib = core.createNode({parent: rootNode, base: FMU.FMU_Library});
+        // set the name attribute of the new FCO to 'NewImportedFMUs'
         core.setAttribute(newFmuLib, 'name', 'NewImportedFMUs');
 
-        // Commit changes.
+
+
+        // Commit changes to the database.
         this.save('Committing changes to database for ImportFMUs plugin', function (err) {
             // FIXME: on error?
 
@@ -53,30 +51,8 @@ define(['plugin/PluginConfig',
                 callback(null, self.result);
             }
         });
-        // End Commit
-
-//        if (selectedNode && typeof selectedNode == FMU.FMU_Library) {
-
-        // NOTE: You should not have any code here!!! Your last executed branch is in the save. Must do everything before the save.
-
-//        if (rootNode) {
-//
-//            core.loadChildren(rootNode, function (err, childNodes) {
-//                var i;
-//                console.log('%j has children::', core.getAttribute(rootNode, 'name'));
-//
-//                for (i = 0; i < childNodes.length; i += 1) {
-//                    console.log('  - %j', core.getAttribute(childNodes[i], 'name'));
-//                }
-//
-//                if (callback) {
-//                    self.result.success = true;
-//                    callback(null, self.result);
-//                }
-//            });
-//        } else {
-//            callback('rootNode is not defined', this.result);
-//        }
+        // End Commit NOTE: You should not have any code here!!!
+        // this.save(commit_msg, ______) should be the last statement
     };
 
     return ImportFMUsPlugin;
