cbde4f82d83823f07d1eb4f3a5ae0197a62efec6
James Klingler <jklingler@isis.vanderbilt.edu>
2014-04-03 17:55:26 -0500

    FMU importer is functional

--- src/plugins/FMU/ImportFMUs/ImportFMUs.js
+++ src/plugins/FMU/ImportFMUs/ImportFMUs.js
@@ -6,7 +6,7 @@ define(['plugin/PluginConfig',
         'plugin/PluginBase',
         'plugin/ImportFMUs/ImportFMUs/FMU',
         'plugin/ImportFMUs/ImportFMUs/resources/ModelicaBlocks'],
-    function (PluginConfig, PluginBase, FMU, FmuImportList) {
+    function (PluginConfig, PluginBase, FmuMetaTypes, FmuImportList) {
     'use strict';
 
     var ImportFMUsPlugin = function () {
@@ -29,34 +29,53 @@ define(['plugin/PluginConfig',
             fmuList = FmuImportList;
 
         // get all 'possible' object types from the MetaModel
-        this.updateMETA(FMU);
+        this.updateMETA(FmuMetaTypes);
 
-        var flatFolderMap = {};
+        var flatFolderMap = {}; // key is dot-seperated path in project, value is the corresponding node
 
         // create an FCO within the rootNode, which is an instance of FMU.FMU_Library
-        var newFmuLib = core.createNode({parent: rootNode, base: FMU.FMU_Library});
+        var newFmuLib = core.createNode({parent: rootNode, base: FmuMetaTypes.FMU_Library});
         // set the name attribute of the new FCO to 'NewImportedFMUs'
-        core.setAttribute(newFmuLib, 'name', 'NewImportedFMUs');
+        var newImportsFolderName = 'NewImportedFMUs';
+        core.setAttribute(newFmuLib, 'name', newImportsFolderName);
 
-        flatFolderMap['NewImportedFMUs'] = '/' + newFmuLib.relid;
+        flatFolderMap[newImportsFolderName] = newFmuLib;
 
         var index;
         var numberOfFmusToImport = fmuList.length;
 
         for (index = 0; index < numberOfFmusToImport; index += 1){
             var fmuData = fmuList[index],
+                newFMU,
                 modelicaClass = fmuData.ModelicaClass,
-                fmuPathWithinProject = modelicaClass.split('.'),
-                folders = fmuPathWithinProject.slice(0, -1).join('.'),      // get the folder structure
-                fmuName = fmuPathWithinProject.slice(-1)[0];                // get the new FMU name
+                fmuFilePath = fmuData.FMU_package,
+                splitModelicaClass = modelicaClass.split('.'),
+                parentPackagePath = splitModelicaClass.slice(0, -1).join('.'),  // get the parent folder path
+                fmuName = splitModelicaClass.slice(-1)[0];                      // get the new FMU name
 
 
-            // create an FCO within the the newFmuLib, which is an instance of FMU.FMU
-            var newFMU = core.createNode({parent: newFmuLib, base: FMU.FMU});
-            // set the name attribute of the new FCO to 'fmuName'
-            core.setAttribute(newFMU, 'name', fmuName);
+            if (splitModelicaClass.length == 0) {
+                fmuName = fmuFilePath.split('\\').slice(-1)[0].split('.')[0];
+
+                // create an FCO within the the newFmuLib, which is an instance of FMU.FMU
+                newFMU = core.createNode({parent: newFmuLib, base: FmuMetaTypes.FMU});
+                // set the name attribute of the new FCO to 'fmuName'
+                core.setAttribute(newFMU, 'name', fmuName);
+
+                ImportFMUsPlugin.buildFmuFromJson(core, newFMU, fmuData, FmuMetaTypes)
+
+            } else {
+                var fullPackagePathWithinProject = newImportsFolderName + '.' + parentPackagePath;
+                var parentFolder = ImportFMUsPlugin.makeFolders(core, fullPackagePathWithinProject, flatFolderMap, FmuMetaTypes);
+
+                // create an FCO within the the newFmuLib, which is an instance of FMU.FMU
+                newFMU = core.createNode({parent: parentFolder, base: FmuMetaTypes.FMU});
+                // set the name attribute of the new FCO to 'fmuName'
+                core.setAttribute(newFMU, 'name', fmuName);
+
+                ImportFMUsPlugin.buildFmuFromJson(core, newFMU, fmuData, FmuMetaTypes)
+            }
         }
-
 
         // Commit changes to the database.
         this.save('Committing changes to database for ImportFMUs plugin', function (err) {
@@ -73,13 +92,83 @@ define(['plugin/PluginConfig',
         // this.save(commit_msg, ______) should be the last statement
     };
 
-    // This should check if a folder (fco?) exists and create it (recursively) if necessary
-    ImportFMUsPlugin.makeFolders = function (modelicaPackagePath, existingFolderMap, core, FMU) {
+    // Recursively create FmuLibrary objects
+    ImportFMUsPlugin.makeFolders = function (core, modelicaPackagePath, existingFolderMap, FmuTypes) {
+        var desiredFolderNode;
 
-        var existingFolderId = existingFolderMap[modelicaPackagePath];
+        if (modelicaPackagePath in existingFolderMap) {
+            // the desired folder already exists; return it
+            desiredFolderNode = existingFolderMap[modelicaPackagePath];
+            return desiredFolderNode;
 
-        if (existingFolder) {return;};
+        } else {
+            // the desired folder does not exist; create and return it
 
+            // get the parent package and see if it exists (recursion)
+
+            var parentPackagePath = modelicaPackagePath.split('.').slice(0, -1).join('.');
+            var thisPackageName = modelicaPackagePath.split('.').slice(-1)[0];
+
+            var parentFolder = ImportFMUsPlugin.makeFolders(core, parentPackagePath, existingFolderMap, FmuTypes);
+
+            var newFmuLibNode = core.createNode({parent: parentFolder, base: FmuMetaTypes.FMU_Library});
+            core.setAttribute(newFmuLibNode, 'name', thisPackageName);
+
+            existingFolderMap[modelicaPackagePath] = newFmuLibNode;
+
+            return newFmuLibNode;
+        }
+    };
+
+    ImportFMUsPlugin.buildFmuFromJson = function (core, newFmuObject, fmuData, FmuTypes) {
+        var parameters = fmuData.Parameters,
+            inputs = fmuData.Inputs,
+            outputs = fmuData.Outputs,
+            index;
+
+        core.setAttribute(newFmuObject, 'fmu_path', fmuData.FMU_package);
+
+        for (index = 0; index < parameters.length; index += 1){
+            var paramData = parameters[index];
+
+            var newParamObject = core.createNode({parent: newFmuObject, base: FmuTypes.Parameter});
+            core.setAttribute(newParamObject, 'name', paramData.name);
+
+            if (paramData.description) {
+                core.setAttribute(newParamObject, 'description', paramData.description);
+            }
+
+            if (paramData.startValue) {
+                core.setAttribute(newParamObject, 'value', paramData.startValue);
+                core.setAttribute(newParamObject, 'default_value', paramData.startValue);
+            }
+            // TODO: put the parameters in the middle of the canvas
+        }
+
+        for (index = 0; index < inputs.length; index += 1){
+            var inputData = inputs[index];
+
+            var newInputPortObject = core.createNode({parent: newFmuObject, base: FmuTypes.Input});
+            core.setAttribute(newInputPortObject, 'name', inputData.name);
+
+            if (inputData.description) {
+                core.setAttribute(newInputPortObject, 'description', inputData.description);
+            }
+
+            // TODO: put the inputs on the left-hand side of the canvas
+        }
+
+        for (index = 0; index < outputs.length; index += 1){
+            var outputData = outputs[index];
+
+            var newInputPortObject = core.createNode({parent: newFmuObject, base: FmuTypes.Input});
+            core.setAttribute(newInputPortObject, 'name', outputData.name);
+
+            if (outputData.description) {
+                core.setAttribute(newInputPortObject, 'description', outputData.description);
+            }
+            // TODO: put the outputs on the righthand side of the canvas
+        }
     };
 
     return ImportFMUsPlugin;
