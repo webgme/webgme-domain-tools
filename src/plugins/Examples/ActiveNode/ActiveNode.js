4a55cc5c4ff6ccf529a0d223b0e8b4060c3843da
pmeijer <patrik85@isis.vanderbilt.edu>
2014-05-01 16:12:17 -0500

    Return with Error object if xml parsing failed.

--- src/plugins/Examples/ActiveNode/ActiveNode.js
+++ src/plugins/Examples/ActiveNode/ActiveNode.js
@@ -19,10 +19,14 @@ define(['plugin/PluginConfig', 'plugin/PluginBase', 'xmljsonconverter'], functio
 
     Logger.prototype.main = function (callback) {
         var self = this,
-            xml2json = new Converter.Xml2json({skipWSText: true,
-                                                  arrayElements: {ttt: true}}),
-            obj = xml2json.convertFromStr('<xml>Hello, <who name="world">world<ttt attr="4"> \n\r  </ttt></who>!</xml>');
-        self.logger.info(JSON.stringify(obj, null, 4));
+            xml2json = new Converter.Xml2json({skipWSText: true, arrayElements: {ttt: true}}),
+            convertedObj = xml2json.convertFromStr('<xml>Hello, <who name="world">world<ttt attr="4"> \n\r  </ttt></who>!</xml>');
+        if (convertedObj instanceof Error) {
+            self.result.setSuccess(false);
+            callback('Xml parsing failed with error: ' + convertedObj.message, self.result);
+            return;
+        }
+        self.logger.info(JSON.stringify(convertedObj, null, 4));
 
         if (!self.activeNode) {
             self.result.setSuccess(false);
@@ -33,7 +37,7 @@ define(['plugin/PluginConfig', 'plugin/PluginBase', 'xmljsonconverter'], functio
         self.logger.info('name : ' + self.core.getAttribute(self.activeNode, 'name'));
         self.logger.info('path : ' + self.core.getPath(self.activeNode));
         self.logger.info('GUID : ' + self.core.getGuid(self.activeNode));
-        self.createMessage(self.activeNode, JSON.stringify(obj, null, 0));
+        self.createMessage(self.activeNode, JSON.stringify(convertedObj, null, 0));
         self.result.setSuccess(true);
         callback(null, self.result);
     };
