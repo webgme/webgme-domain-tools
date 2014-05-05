937b75392449fbac8c2103a0f49e4126063807fb
pmeijer <patrik85@isis.vanderbilt.edu>
2014-05-05 16:05:33 -0500

    GH-15 Removal of old example interpreters.

--- src/plugins/Examples/UsingTemplates/Templates/Templates.js
+++
@@ -1,6 +0,0 @@
-/* Generated file based on ejs templates */
-define([], function() {
-    return {
-    "sample.html.ejs": "<html>\r\n\r\n<body>\r\n<div>name = <%= name %></div>\r\n</body>\r\n</html>",
-    "sample.py.ejs": "name = <%= name %>"
-}});
