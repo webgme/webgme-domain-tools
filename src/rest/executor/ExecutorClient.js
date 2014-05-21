17cd08e8c66df51c43f78a24da57b681965543e4
Zsolt Lattmann <lattmann@isis.vanderbilt.edu>
2014-05-21 15:53:43 -0500

    Add ExecutorClient to execute job from plugin

---
+++ src/rest/executor/ExecutorClient.js
@@ -0,0 +1,80 @@
+/**
+ * Created by Zsolt on 5/21/2014.
+ * 
+ * THIS IS A THROW AWAY CODE AND IMPLEMENTATION.
+ *
+ * TEMPORARY CODE AND IMPLEMENTATION.
+ *
+ */
+
+define([], function () {
+
+    var ExecutorClient = function () {
+
+        // TODO: TOKEN???
+        this.executorUrl = '/rest/external/executor/'; // TODO: any ways to ask for this or get it from the configuration?
+    };
+
+    ExecutorClient.prototype.getInfoURL = function (hash) {
+        var metadataBase = this.executorUrl + 'info';
+        if (hash) {
+            return metadataBase + '/' + hash;
+        } else {
+            return metadataBase;
+        }
+    };
+
+
+    ExecutorClient.prototype.getCreateURL = function (hash) {
+        var metadataBase = this.executorUrl + 'create';
+        if (hash) {
+            return metadataBase + '/' + hash;
+        } else {
+            return metadataBase;
+        }
+    };
+
+    ExecutorClient.prototype.createJob = function (hash, callback) {
+        var oReq = new XMLHttpRequest();
+        oReq.open("POST", this.getCreateURL(hash), true);
+        oReq.onload = function (oEvent) {
+            // Uploaded.
+            var response = JSON.parse(oEvent.target.response);
+            callback(null, response);
+        };
+
+        // data is a file object or blob
+        oReq.send();
+    };
+
+
+    ExecutorClient.prototype.getInfo = function (hash, callback) {
+        var oReq = new XMLHttpRequest();
+        oReq.open("GET", this.getInfoURL(hash), true);
+        oReq.onload = function (oEvent) {
+            // Uploaded.
+            var response = JSON.parse(oEvent.target.response);
+            // TODO: handle error
+            callback(null, response);
+        };
+
+        // data is a file object or blob
+        oReq.send();
+    };
+
+    ExecutorClient.prototype.getAllInfo = function (callback) {
+        var oReq = new XMLHttpRequest();
+        oReq.open("GET", this.getInfoURL(), true);
+        oReq.onload = function (oEvent) {
+            // Uploaded.
+            var response = JSON.parse(oEvent.target.response);
+            // TODO: handle error
+            callback(null, response);
+        };
+
+        // data is a file object or blob
+        oReq.send();
+    };
+
+    return ExecutorClient;
+});
