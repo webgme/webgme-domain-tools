fded62b7ab6ac10ae785201e36ad464d18832888
zhangpn <zhangpn@isis.vanderbilt.edu>
2014-06-17 11:53:42 -0500

    GH-18 Added NetLabelPanel specific event handles

---
+++ src/widgets/NetLabel/NetLabelWidget.Constants.js
@@ -0,0 +1,40 @@
+"use strict";
+/*
+ * STRING CONSTANT DEFINITIONS USED IN DESIGNER DIAGRAM
+ */
+
+define(['js/Constants'], function (CONSTANTS) {
+
+    //return string constants
+    return {
+        /*
+         * TERRITORY EVENTS
+         */
+        SELF : "__SELF__",
+
+        /*
+         * CLASS DEFINITIONS
+         */
+        DESIGNER_ITEM_CLASS : "designer-item",
+        DESIGNER_CONNECTION_CLASS : "designer-connection",
+        CONNECTION_DRAGGABLE_END_CLASS : "c-d-end",
+        CONNECTOR_CLASS : "connector",
+        CONNECTION_END_SRC : 'src',
+        CONNECTION_END_DST : 'dst',
+        CONNECTION_CONTAINER_SVG_CLASS : 'connection-container',
+        HIGHLIGHT_MODE_CLASS: 'highlight-mode',
+        ITEM_HIGHLIGHT_CLASS: 'highlighted',
+        DROP_REGION_CLASS: 'drop-region',
+        DROP_REGION_ACCEPT_DROPPABLE_CLASS: 'accept-droppable',
+        DROP_REGION_REJECT_DROPPABLE_CLASS: 'reject-droppable',
+
+        /*DOM ELEMENT ATTRIBUTES*/
+        DATA_ITEM_ID : 'data-oid',
+        DATA_SUBCOMPONENT_ID : 'data-sid',
+
+        /*
+         * ROTATINO RESET CONSTANTS
+         */
+        ROTATION_RESET: 'reset'
+    };
+});
