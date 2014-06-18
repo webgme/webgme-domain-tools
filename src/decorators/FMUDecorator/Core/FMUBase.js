6c0b95a06440df7fa6c0193b62b487307656d48f
James Klingler <jklingler@isis.vanderbilt.edu>
2014-06-18 12:03:29 -0500

    attempt to get child info for FMU decorator

--- src/decorators/FMUDecorator/Core/FMUBase.js
+++ src/decorators/FMUDecorator/Core/FMUBase.js
@@ -106,6 +106,143 @@ define(['js/NodePropertyNames',
         }
     };
 
+    FMUBase.prototype._updatePorts = function () {
+        var portId,
+            gmeID = this._metaInfo[CONSTANTS.GME_ID],
+            client = this._control._client,
+            nodeObj = client.getNode(gmeID),
+            childrenIDs = nodeObj ?  nodeObj.getChildrenIds() : [],
+            len = childrenIDs.length,
+            META_TYPES = FMUMETA.META_TYPES,
+            SVGWidth = parseInt(this.skinParts.$svg.attr('width')),
+            SVGHeight = parseInt(this.skinParts.$svg.attr('height')),
+            PortWidth = FMUDecoratorConstants.PORT_WIDTH;
+
+        var port,
+            svgPort;
+
+        // var portName = port.getAttribute(nodePropertyNames.Attributes.name);
+        // var svgPort = this.skinParts.$svg.find('#' + portName);
+
+        var isFmu = METAAspectHelper.isMETAType(gmeID, META_TYPES.FMU);
+        if (isFmu && len){
+
+            this._bboxes = [];
+            this.skinParts.$connectorContainer.empty();
+            var modelSVG = this.skinParts.$svg;
+            var portsSvgs = modelSVG.find('g');
+            var childrenNames = [];
+            var childSvgHash;
+            for(var i = 0; i < len; i++){
+                var childName = client.getNode(childrenIDs[i]).getAttribute(nodePropertyNames.Attributes.name);
+
+                if (client.getNode(childrenIDs[i]).getAttribute(nodePropertyNames.Attributes.hasOwnProperty('svg'))) {
+                    childSvgHash = client.getNode(childrenIDs[i]).getAttribute(nodePropertyNames.Attributes.svg);
+                    //childrenNames.push(client.getNode(childrenIDs[i]).getAttribute("name"));
+                    for(var j = 0; j < portsSvgs.length; j++){
+                        if (childName === $(portsSvgs[j]).attr('id')){
+                            var portSvg = $(portsSvgs[j]);
+                            var connDec = $('<div/>', {'class': DiagramDesignerWidgetConstants.CONNECTOR_CLASS});
+
+
+                            // N.B. it's using the x, y assuming rectangle! This is not always the case.
+                            var bbox = {
+                                'x': parseInt(($(portSvg.find('rect'))).attr('x')),
+                                'y': parseInt(($(portSvg.find('rect'))).attr('y')),
+                                'width': 20,
+                                'height': 20};
+
+                            connDec.css({'left':bbox.x, 'top':bbox.y, 'width':bbox.width, 'height':bbox.height});
+
+                            svgPort = this.skinParts.$svg.find('#' + childName);
+                            if (svgPort.length > 0) {
+                                svgPort = svgPort[0];
+                                var svgInfo = $(svgPort).find('#info');
+                                if (childSvgHash) {
+                                    connDec.attr('title', childSvgHash);
+                                }
+                            }
+
+                            if (this.hostDesignerItem) {
+                                this.hostDesignerItem.registerConnectors(connDec, childrenIDs[i]);
+                                this.hostDesignerItem.registerSubcomponent(childrenIDs[i], {"GME_ID": childrenIDs[i]});
+                                this._bboxes[childrenIDs[i]] = bbox;
+                            } else {
+                                this.logger.error("Decorator's hostDesignerItem is not set");
+                            }
+
+                            this.skinParts.$connectorContainer.append(connDec);
+                        }
+                    }
+                }
+            }
+
+        } else {
+            len = 4;
+            this._connectionAreas = {};
+            this.skinParts.$connectorContainer.empty();
+
+            // positioning the connectors' connection areas
+
+            // TOP
+            this._connectionAreas[0] = {
+                "x1": SVGWidth / 2,
+                "y1": 0
+            }
+            // BOTTOM
+            this._connectionAreas[1] = {
+                "x1": SVGWidth / 2,
+                "y1": SVGHeight
+            }
+            // LEFT
+            this._connectionAreas[2] = {
+                "x1": 0,
+                "y1": SVGHeight / 2
+            }
+            // RIGHT
+            this._connectionAreas[3] = {
+                "x1": SVGWidth,
+                "y1": SVGHeight / 2
+            }
+
+            while(len--) {
+
+                portId = 3 - len;
+                // render connector
+                var connectorE = $('<div/>', {'class': DiagramDesignerWidgetConstants.CONNECTOR_CLASS});
+
+                if (portId === 3) {
+                    connectorE.addClass(FMUDecoratorConstants.RIGHT_PORT_CLASS);
+                } else if (portId === 2) {
+                    connectorE.addClass(FMUDecoratorConstants.LEFT_PORT_CLASS);
+                } else if (portId === 1 || portId === 4 || portId === 5) {
+                    connectorE.addClass(FMUDecoratorConstants.BOTTOM_PORT_CLASS);
+                } else {
+                    connectorE.addClass(FMUDecoratorConstants.TOP_PORT_CLASS);
+                }
+
+                connectorE.css({
+                    'top': this._connectionAreas[portId].y1 - PortWidth,
+                    'left': this._connectionAreas[portId].x1 - PortWidth
+                });
+
+                if (this._displayConnectors) {
+
+                    // register connectors for creating connections
+                    if (this.hostDesignerItem) {
+                        this.hostDesignerItem.registerConnectors(connectorE);
+                    } else {
+                        this.logger.error("Decorator's hostDesignerItem is not set");
+                    }
+
+                    this.skinParts.$connectorContainer.append(connectorE);
+                }
+            }
+        }
+    };
+
+    
+    
     /**
      * Renders the object based on the meta type.
      * @private
