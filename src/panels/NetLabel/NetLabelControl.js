90d5b5c32f5715ab985c31291786d543a123a48b
zhangpn <zhangpn@isis.vanderbilt.edu>
2014-06-24 09:16:35 -0500

    GH-18 Restructured & cleaned up code

--- src/panels/NetLabel/NetLabelControl.js
+++ src/panels/NetLabel/NetLabelControl.js
@@ -53,9 +53,7 @@ define(['js/Panels/ModelEditor/ModelEditorControl',
     NetLabelControl.prototype._onUpdate = function (gmeID, objDesc) {
         var componentID,
             len,
-            decClass,
-            objId,
-            sCompId;
+            decClass;
 
         //self or child updated
         //check if the updated object is the opened node
@@ -81,65 +79,8 @@ define(['js/Panels/ModelEditor/ModelEditorControl',
                                 objDesc.aspect = this._selectedAspect;
 
                                 this.designerCanvas.updateDesignerItem(componentID, objDesc);
+                                this._onItemNameUpdate(componentID, objDesc);
 
-                                // ***************updating netLabels when components names change*********************
-                                // todo: refinement
-                                var connections2update = this.designerCanvas.connectionIDbyEndID[componentID];
-                                if (connections2update) {
-                                    var counter,
-                                        connComponentId,
-                                        item,
-                                        labels2update,
-                                        ctr;
-                                    if (connections2update.__SELF__) {
-                                        counter = connections2update.__SELF__.length;
-                                        while (counter--) {
-                                            connComponentId = connections2update.__SELF__[counter];
-                                            item = this.designerCanvas.items[connComponentId];
-                                            labels2update = item.diagramDesigner.$el.find('[connid^="' + connComponentId + '"]');
-                                            ctr = labels2update.length;
-                                            while (ctr--) {
-                                                if (labels2update[ctr].getAttribute("id") === componentID) {
-                                                    labels2update[ctr].textContent = objDesc.name;
-                                                }
-                                            }
-                                        }
-                                    }
-
-                                    // update label names when a subcomponent's parent name changes
-                                    for (sCompId in connections2update) {
-                                        if (connections2update.hasOwnProperty(sCompId)) {
-                                            var cObjArray = connections2update[sCompId];
-                                            counter = cObjArray.length;
-                                            while (counter--) {
-                                                connComponentId = cObjArray[counter];
-                                                item = this.designerCanvas.items[connComponentId];
-                                                labels2update = item.diagramDesigner.$el.find('[connid^="' + connComponentId + '"]');
-                                                ctr = labels2update.length;
-                                                while (ctr--) {
-                                                    if (labels2update[ctr].getAttribute("id") === sCompId) {
-                                                        var subStr = labels2update[ctr].textContent.substr(labels2update[ctr].textContent.indexOf('.'), labels2update[ctr].textContent.length);
-                                                        labels2update[ctr].textContent = objDesc.name + subStr;
-                                                    }
-                                                }
-                                            }
-
-                                        }
-                                    }
-                                    counter = connections2update.length;
-                                    while (counter--) {
-                                        connComponentId = connections2update.__SELF__[counter];
-                                        item = this.designerCanvas.items[connComponentId];
-                                        labels2update = item.diagramDesigner.$el.find('[connid^="' + connComponentId + '"]');
-                                        ctr = labels2update.length;
-                                        while (ctr--) {
-                                            if (labels2update[ctr].getAttribute("id") === componentID) {
-                                                labels2update[ctr].textContent = objDesc.name;
-                                            }
-                                        }
-                                    }
-                                }
-                                // ***********************************************************************************
                             }
                         }
                     }
@@ -206,123 +147,69 @@ define(['js/Panels/ModelEditor/ModelEditorControl',
         }
     };
 
-//
-//    NetLabelControl.prototype._onLoad = function (gmeID, objD) {
-//        var uiComponent,
-//            decClass,
-//            objDesc,
-//            sources = [],
-//            destinations = [],
-//            getDecoratorTerritoryQueries,
-//            territoryChanged = false,
-//            self = this;
-//
-//        getDecoratorTerritoryQueries = function (decorator) {
-//            var query,
-//                entry;
-//
-//            if (decorator) {
-//                query = decorator.getTerritoryQuery();
-//
-//                if (query) {
-//                    for (entry in query) {
-//                        if (query.hasOwnProperty(entry)) {
-//                            self._selfPatterns[entry] = query[entry];
-//                            territoryChanged = true;
-//                        }
-//                    }
-//                }
-//            }
-//        };
-//
-//
-//        //component loaded
-//        //we are interested in the load of sub_components of the opened component
-//        if (this.currentNodeInfo.id !== gmeID) {
-//            if (objD) {
-//                if (objD.parentId == this.currentNodeInfo.id) {
-//                    objDesc = _.extend({}, objD);
-//                    this._GmeID2ComponentID[gmeID] = [];
-//
-//                    if (objDesc.kind === "MODEL") {
-//
-//                        this._GMEModels.push(gmeID);
-//
-//                        decClass = this._getItemDecorator(objDesc.decorator);
-//
-//                        objDesc.decoratorClass = decClass;
-//                        objDesc.control = this;
-//                        objDesc.metaInfo = {};
-//                        objDesc.metaInfo[CONSTANTS.GME_ID] = gmeID;
-//                        objDesc.preferencesHelper = PreferencesHelper.getPreferences();
-//                        objDesc.aspect = this._selectedAspect;
-//
-//                        uiComponent = this.designerCanvas.createDesignerItem(objDesc);
-//
-//
-//                        this._GmeID2ComponentID[gmeID].push(uiComponent.id);
-//                        this._ComponentID2GmeID[uiComponent.id] = gmeID;
-//
-//                        getDecoratorTerritoryQueries(uiComponent._decoratorInstance);
-//
-//                    }
-//
-//                    if (objDesc.kind === "CONNECTION") {
-//
-//                        this._GMEConnections.push(gmeID);
-//
-//                        var srcDst = this._getAllSourceDestinationPairsForConnection(objDesc.source, objDesc.target);
-//                        sources = srcDst.sources;
-//                        destinations = srcDst.destinations;
-//
-//                        var k = sources.length;
-//                        var l = destinations.length;
-//
-//                        if (k > 0 && l > 0) {
-//                            while (k--) {
-//                                while (l--) {
-//                                    objDesc.srcObjId = sources[k].objId;
-//                                    objDesc.srcSubCompId = sources[k].subCompId;
-//                                    objDesc.dstObjId = destinations[l].objId;
-//                                    objDesc.dstSubCompId = destinations[l].subCompId;
-//                                    objDesc.reconnectable = true;
-//                                    objDesc.editable = true;
-//
-//                                    delete objDesc.source;
-//                                    delete objDesc.target;
-//
-//                                    _.extend(objDesc, this.getConnectionDescriptor(gmeID));
-//                                    uiComponent = this.designerCanvas.createConnection(objDesc);
-//
-//                                    this.logger.debug('Connection: ' + uiComponent.id + ' for GME object: ' + objDesc.id);
-//
-//                                    this._GmeID2ComponentID[gmeID].push(uiComponent.id);
-//                                    this._ComponentID2GmeID[uiComponent.id] = gmeID;
-//                                }
-//                            }
-//                        } else {
-//                            //the connection is here, but no valid endpoint on canvas
-//                            //save the connection
-//                            this._delayedConnections.push(gmeID);
-//                        }
-//                    }
-//                } else {
-//                    //supposed to be the grandchild of the currently open node
-//                    //--> load of port
-//                    /*if(this._GMEModels.indexOf(objD.parentId) !== -1){
-//                     this._onUpdate(objD.parentId,this._getObjectDescriptor(objD.parentId));
-//                     }*/
-//                    this._checkComponentDependency(gmeID, CONSTANTS.TERRITORY_EVENT_LOAD);
-//                }
-//            }
-//        } else {
-//            //currently opened node
-//            this._updateSheetName(objD.name);
-//            this._updateAspects();
-//        }
-//
-//        return territoryChanged;
-//
-//    };
+    NetLabelControl.prototype._onItemNameUpdate = function (componentID, objDesc) {
+        // ***************updating netLabels when components names change*********************
+        // todo: refinement
+        var connections2update = this.designerCanvas.connectionIDbyEndID[componentID],
+            counter,
+            ctr,
+            connComponentId,
+            item,
+            sCompId,
+            labels2update,
+            cObjArray;
+
+        if (connections2update) {
+            if (connections2update.__SELF__) {
+                counter = connections2update.__SELF__.length;
+                while (counter--) {
+                    connComponentId = connections2update.__SELF__[counter];
+                    item = this.designerCanvas.items[connComponentId];
+                    labels2update = item.diagramDesigner.$el.find('[connid^="' + connComponentId + '"]');
+                    ctr = labels2update.length;
+                    while (ctr--) {
+                        if (labels2update[ctr].getAttribute("id") === componentID) {
+                            labels2update[ctr].textContent = objDesc.name;
+                        }
+                    }
+                }
+            }
+
+            // update label names when a subcomponent's parent name changes
+            for (sCompId in connections2update) {
+                if (connections2update.hasOwnProperty(sCompId)) {
+                    cObjArray = connections2update[sCompId];
+                    counter = cObjArray.length;
+                    while (counter--) {
+                        connComponentId = cObjArray[counter];
+                        item = this.designerCanvas.items[connComponentId];
+                        labels2update = item.diagramDesigner.$el.find('[connid^="' + connComponentId + '"]');
+                        ctr = labels2update.length;
+                        while (ctr--) {
+                            if (labels2update[ctr].getAttribute("id") === sCompId) {
+                                var subStr = labels2update[ctr].textContent.substr(labels2update[ctr].textContent.indexOf('.'), labels2update[ctr].textContent.length);
+                                labels2update[ctr].textContent = objDesc.name + subStr;
+                            }
+                        }
+                    }
+
+                }
+            }
+            counter = connections2update.length;
+            while (counter--) {
+                connComponentId = connections2update.__SELF__[counter];
+                item = this.designerCanvas.items[connComponentId];
+                labels2update = item.diagramDesigner.$el.find('[connid^="' + connComponentId + '"]');
+                ctr = labels2update.length;
+                while (ctr--) {
+                    if (labels2update[ctr].getAttribute("id") === componentID) {
+                        labels2update[ctr].textContent = objDesc.name;
+                    }
+                }
+            }
+        }
+        // ***********************************************************************************
+    };
+
     return NetLabelControl;
 });
