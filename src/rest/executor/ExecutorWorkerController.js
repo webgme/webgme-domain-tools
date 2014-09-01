5a4d143c291b4459d0a2e87154f8ad8954d79f30
Zsolt Lattmann <lattmann@isis.vanderbilt.edu>
2014-09-01 10:52:35 -0500

    Style the executor slave's html page.

--- src/rest/executor/ExecutorWorkerController.js
+++ src/rest/executor/ExecutorWorkerController.js
@@ -30,18 +30,24 @@ define([], function () {
     };
 
     ExecutorWorkerController.prototype.initTestData = function () {
-        var self = this;
-        self.$scope.jobs = { job1: { status: 'asdf' },
-            job2: { status: 'asdf' }};
+        var self = this,
+            i,
+            statuses = [ 'CREATED', 'SUCCESS', 'FAILED_TO_EXECUTE', 'FAILED_TO_GET_SOURCE_METADATA', 'FAILED_SOURCE_COULD_NOT_BE_OBTAINED',
+                'FAILED_CREATING_SOURCE_ZIP', 'FAILED_UNZIP', 'FAILED_EXECUTOR_CONFIG', 'FAILED_TO_ARCHIVE_FILE', 'FAILED_TO_SAVE_JOINT_ARTIFACT', 'FAILED_TO_ADD_OBJECT_HASHES',
+                'FAILED_TO_SAVE_ARTIFACT'];
 
-        for (var i = 0; i < 10; i += 1) {
+        self.$scope.jobs = { };
+
+        for (i = 0; i < 30; i += 1) {
             self.$scope.jobs['/' + i] = {
-                status: (i % 3) ? 'OK' : 'FAILED',
+//                status: (i % 3) ? 'OK' : 'FAILED',
                 hash: i,
                 url: '',
                 resultHash: i + 10000
             };
-        };
+
+            self.$scope.jobs['/' + i].status = statuses[Math.floor(Math.random() * statuses.length)];
+        }
 
     };
 
