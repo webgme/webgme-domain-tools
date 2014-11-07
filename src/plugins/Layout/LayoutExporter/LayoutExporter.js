50920a99c8d5251a352cc4e0a15777ee7c8c606c
zhangpn <zhangpn@isis.vanderbilt.edu>
2014-11-07 15:22:18 -0600

    GH-20 Added some more code logic

--- src/plugins/Layout/LayoutExporter/LayoutExporter.js
+++ src/plugins/Layout/LayoutExporter/LayoutExporter.js
@@ -38,28 +38,134 @@ define(['plugin/PluginConfig',
 //        core.persist(self.rootNode, function (err) {
 //        });
 //        newRootHash = core.getHash(self.rootNode);
-//        console.info(self.project.makeCommit);
+//        console.info("Plugin updated model");
 //        result = {'commitHash': self.commitHash};
 //        result.commitHash = self.project.makeCommit([result.commitHash], newRootHash, 'Plugin updated the model.', function (err) {
 //        });
+
 
         if (!selectedNode) {
             callback('selectedNode is not defined', self.result);
             return;
         }
+
+        // after all children are visited
         afterAllVisited = function (err) {
+            var i,
+                error,
+                counter = self.wiresToAdd.length,
+                afterWireAdded;
             if (err) {
                 callback(err, self.result);
                 return;
             }
+            self.logger.warning('Visited all children!');
             self.saveResults(callback);
+
         };
-        self.visitFromNode(selectedNode, afterAllVisited);
+
+        self.getChildrenFromNode(selectedNode, afterAllVisited);
+
+        console.info("Plugin completed");
     };
 
-    LayoutExporterPlugin.prototype.visitFromNode = function (node, callback) {
+    LayoutExporterPlugin.prototype.getChildrenFromNode = function (node, callback) {
+        var self = this,
+            afterLoading;
+
+        afterLoading = function (err, children) {
+            var i,
+                itrCallback,
+                error = '';
+            if (err) {
+                callback('Could not load children for object, err: ' + err);
+                return;
+            }
+
+            itrCallback = function (err) {
+                error = err ? error += err : error;
+            };
+
+            if (children.length === 0) {
+                itrCallback(null);
+            } else {
+                for (i = 0; i < children.length; i += 1) {
+                    self.atNode(children[i], function (err, obj, isConnection) {
+                        // obj is the resulting component or connection object from atNode()
+                        // store obj
+
+                    });
+                }
+            }
+        };
+
+        // load activeNode's children
+        self.core.loadChildren(node, afterLoading);
+    };
+
+    LayoutExporterPlugin.prototype.atNode = function (node, callback) {
+
+        var self = this,
+            core = self.core,
+            SRCPTR = "src",
+            DSTPTR = "dst",
+            ptrArray = core.getOwnPointerNames(node),
+            srcPath,
+            dstPath,
+            srcObj,
+            dstObj,
+            afterLoadingSrc,
+            afterLoadingDst;
+
+        afterLoadingSrc = function (err, node) {
+
+            if (err) {
+                callback(err, null, null);
+                return;
+            }
+
+            // if src node is a port, then return srcObj as its parent node
+
+            // else return node as srcObj
+
+        };
+
+        afterLoadingDst = function (err, node) {
+
+            if (err) {
+                callback(err, null, null);
+                return;
+            }
+
+            // if dst node is a port, then return srcObj as its parent node
+
+            // else return node as dstObj
+
+        };
+
+        // check if node is a connection
+        // todo: find a better way to get connections...
+        if (ptrArray.indexOf(SRCPTR) > -1 && ptrArray.indexOf(DSTPTR) > - 1) {
+            // get source object and dst object:
+
+            srcPath = core.getPointerPath(node, SRCPTR);
+            dstPath = core.getPointerPath(node, DSTPTR);
+
+            core.loadByPath(self.rootNode, srcPath, afterLoadingSrc);
+            core.loadByPath(self.rootNode, dstPath, afterLoadingDst);
+
+            // name, srcid, dstid, pathpoints
+            // callback(null, conn, true);
+        } else {
+
+            // node is a component, get id, name, position, size if there's one
+            // get ports if component has ports
+            // callback(null, comp, false);
+        }
+
 
     };
+
 
     return LayoutExporterPlugin;
 });
