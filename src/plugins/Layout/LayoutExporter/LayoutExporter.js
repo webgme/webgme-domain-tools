9e20f6c9ccd08dda0b76feadaf898d16f61bdfe9
zhangpn <zhangpn@isis.vanderbilt.edu>
2014-11-10 14:40:07 -0600

    GH-20 Extracted basic component, connection info

--- src/plugins/Layout/LayoutExporter/LayoutExporter.js
+++ src/plugins/Layout/LayoutExporter/LayoutExporter.js
@@ -6,13 +6,18 @@ define(['plugin/PluginConfig',
 
     'use strict';
 
+    var NAME = 'name',
+        ISPORT = 'isPort',
+        DELIM = '=>';
+
     var LayoutExporterPlugin = function () {
         PluginBase.call(this);
         this.modelID = 0;
         this.diagram = {};
         this.idLUT = {};
         this.error = '';
-        this.childrenLUT = {};
+        this.connections = [];
+        this.components = [];
     };
 
     LayoutExporterPlugin.prototype = Object.create(PluginBase.prototype);
@@ -28,7 +33,6 @@ define(['plugin/PluginConfig',
             selectedNode = self.activeNode,
             afterAllVisited;
 
-
         // uncomment this to enable updating "validPlugin" field
 //        var newRootHash,
 //            result,
@@ -43,7 +47,6 @@ define(['plugin/PluginConfig',
 //        result.commitHash = self.project.makeCommit([result.commitHash], newRootHash, 'Plugin updated the model.', function (err) {
 //        });
 
-
         if (!selectedNode) {
             callback('selectedNode is not defined', self.result);
             return;
@@ -51,22 +54,49 @@ define(['plugin/PluginConfig',
 
         // after all children are visited
         afterAllVisited = function (err) {
-            var i,
-                error,
-                counter = self.wiresToAdd.length,
-                afterWireAdded;
+            var output = {
+                components: self.components,
+                connections: self.connections
+            };
+
             if (err) {
                 callback(err, self.result);
                 return;
             }
             self.logger.warning('Visited all children!');
-            self.saveResults(callback);
+            self.saveResults(output, callback);
 
         };
 
         self.getChildrenFromNode(selectedNode, afterAllVisited);
 
         console.info("Plugin completed");
+    };
+
+    LayoutExporterPlugin.prototype.saveResults = function (obj, callback) {
+        var self = this,
+            core = self.core,
+            artifact = self.blobClient.createArtifact('LayoutExporterOutput'),
+            file = {};
+
+        file[core.getAttribute(self.activeNode, NAME) + ".json"] = JSON.stringify(obj, null, 4);
+
+        artifact.addFiles(file, function (err, hashes) {
+            if (err) {
+                callback(err, self.result);
+                return;
+            }
+            self.logger.warning(hashes.toString());
+            artifact.save(function (err, hash) {
+                if (err) {
+                    callback(err, self.result);
+                    return;
+                }
+                self.result.addArtifact(hash);
+                self.result.setSuccess(true);
+                callback(null, self.result);
+            });
+        });
     };
 
     LayoutExporterPlugin.prototype.getChildrenFromNode = function (node, callback) {
@@ -82,21 +112,16 @@ define(['plugin/PluginConfig',
                 return;
             }
 
-            itrCallback = function (err) {
-                error = err ? error += err : error;
-            };
+            for (i = 0; i < children.length; i += 1) {
+                self.atNode(children[i], function (err) {
+                    if (err) {
+                        // todo: error handling
+                    }
 
-            if (children.length === 0) {
-                itrCallback(null);
-            } else {
-                for (i = 0; i < children.length; i += 1) {
-                    self.atNode(children[i], function (err, obj, isConnection) {
-                        // obj is the resulting component or connection object from atNode()
-                        // store obj
-
-                    });
-                }
+                });
             }
+
+            callback(null);
         };
 
         // load activeNode's children
@@ -110,37 +135,79 @@ define(['plugin/PluginConfig',
             SRCPTR = "src",
             DSTPTR = "dst",
             ptrArray = core.getOwnPointerNames(node),
+            component = {},
+            connection = {},
             srcPath,
             dstPath,
-            srcObj,
-            dstObj,
+            counter = 2,
+            error = "",
             afterLoadingSrc,
-            afterLoadingDst;
+            afterLoadingDst,
+            pushConnection;
 
         afterLoadingSrc = function (err, node) {
+            var isPort = core.getRegistry(node, ISPORT),
+                portId = "",
+                srcObj;
 
             if (err) {
-                callback(err, null, null);
+                pushConnection(err);
                 return;
             }
 
             // if src node is a port, then return srcObj as its parent node
+            if (isPort) {
+                srcObj = core.getParent(node);
+                portId = DELIM + core.getPath(node);
+            } else {
+                // else return node as srcObj
+                srcObj = node;
+            }
 
-            // else return node as srcObj
-
+            connection.srcID = core.getPath(srcObj) + portId;
+            pushConnection(null, connection);
         };
 
         afterLoadingDst = function (err, node) {
+            var isPort = core.getRegistry(node, ISPORT),
+                portId = "",
+                dstObj;
 
             if (err) {
-                callback(err, null, null);
+                pushConnection(err);
                 return;
             }
 
-            // if dst node is a port, then return srcObj as its parent node
+            // if dst node is a port, then return dstObj as its parent node
+            if (isPort) {
+                dstObj = core.getParent(node);
+                portId = DELIM + core.getPath(node);
+            } else {
+                // else return node as dstObj
+                dstObj = node;
+            }
 
-            // else return node as dstObj
+            connection.dstID = core.getPath(dstObj) + portId;
 
+            pushConnection(null, connection);
+        };
+
+        pushConnection = function (err, connection) {
+            // todo: get pathpoints of connection
+            if (err) {
+                error += err;
+            }
+
+            --counter;
+            // waiting on two threads -- srcobj and dstobj threads
+            if (counter === 0) {
+                if (error) {
+                    callback(error);
+                    return;
+                }
+                self.connections.push(connection);
+                callback(null);
+            }
         };
 
         // check if node is a connection
@@ -151,19 +218,25 @@ define(['plugin/PluginConfig',
             srcPath = core.getPointerPath(node, SRCPTR);
             dstPath = core.getPointerPath(node, DSTPTR);
 
+            // name, id, srcid, dstid, pathpoints
+
+            connection.name = core.getAttribute(node, NAME);
+            connection.id = core.getPath(node);
+
             core.loadByPath(self.rootNode, srcPath, afterLoadingSrc);
             core.loadByPath(self.rootNode, dstPath, afterLoadingDst);
 
-            // name, srcid, dstid, pathpoints
-            // callback(null, conn, true);
         } else {
-
             // node is a component, get id, name, position, size if there's one
             // get ports if component has ports
-            // callback(null, comp, false);
+
+            component.name = core.getAttribute(node, NAME);
+            component.id = core.getPath(node);
+            component.position = node.data.reg.position;
+
+            // todo: get size of component, ports if any
+            self.components.push(component);
         }
-
-
     };
 
 
